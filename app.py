c1d70ede72583c5cb70facca35a308e087f2a892
Shkunn <leclercqted@gmail.com>
2021-08-09 16:24:57 +0200

    Auto commit

--- app.py
+++ app.py
@@ -675,5 +675,5 @@ def get_data(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0")
-    # app.run(host="0.0.0.0")
+    # socketio.run(app, host="0.0.0.0")
+    app.run(host="0.0.0.0")
